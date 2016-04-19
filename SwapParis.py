9259f959587e0d06155db4afab25dae05bfbf60f
hylyer <huyong1109@gmail.com>
2016-04-19 18:48:20 +0800

    add swappair

--- SwapParis.py
+++ SwapParis.py
@@ -7,43 +7,54 @@
 #  };
 # 
 
-from ctypes import *
-class struct 
-class Solution {
-public:
-	def createListNode(self, head) {
-		if not L : reutrn None
+
+class ListNode(object):
+	def __init__(self, val):
+		self.val = val 
+		self.next = None 
+
+
+class Solution(object):
+	def createListNode(self, L):
+
+		if not L : 
+			print('input Null')
+			return None
 		resL = ListNode(L[0])
+
 		curNode = resL
-		for val in L[1:]
+		for val in L[1:]:
+
 			curNode.next = ListNode(val)
 			curNode = curNode.next
+		self.printListNode(resL)
 		return resL
+		
 
-	}
-
-	def printListNode(self, head) {
-		while head :
-			print(head.val)
+	def printListNode(self, head):
+		print("List:  [", end="")
+		while head.next :
+			print(head.val, end=",")
+			head = head.next
+		print(head.val, end="")
+		print("]")
 		return 
 
-	}
-    def swapPairs(self, head) {
-        curHead = head #head of current change circle 
-         
-        while  curHead :
-            if curHead.next:
-                curHead.val, curHead.next.val = curHead.next.val, curHead.val
-                curHead = curHead.next.next
-            elif: 
-            	curHead = None
-        return 
-                
-                
-        
-    }
-};
+	def swapPairs(self, head) :
 
+		p1 = guard = ListNode(0)
+		guard.next = head
+
+		try:
+			while True:
+				p0, p1, p2 = p1, p1.next, p1.next.next
+				p0.next, p1.next, p2.next = p2, p2.next, p1
+		except:
+			return guard.next
+testList = range(6)
 sol = Solution()
-TestList = ListNode([1,2,3,4])
-	Solution.swapParis
+print(testList)
+testListNode = sol.createListNode(testList)
+newListNode = sol.swapPairs(testListNode) 
+print('SwapPairs')
+sol.printListNode(newListNode)
